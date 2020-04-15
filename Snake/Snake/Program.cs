f7ca675fe60e1eba544dbd55148bde922d746529
Tatyana <Asta3647@mail.ru>
2020-04-15 14:31:37 +0300

    working with lists

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -6,19 +7,48 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-           /* p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
+            /* Point p1 = new Point(1, 3, '*');
+             p1.Draw(1, 3, '*');
 
-            p1.Draw(1, 3, '*');
+             Point p2 = new Point(4, 5, '#');
+             p2.Draw(4, 5, '#');
 
-            Point p2 = new Point(4, 5, '#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
+             List<int> numList = new List<int>();
+             numList.Add(0);
+             numList.Add(1);
+             numList.Add(2);
 
-            p2.Draw(4, 5, '#');
+             int x = numList[0];
+             int y = numList[1];
+             int c = numList[2];
+
+            foreach(int i in numList)
+             {
+                 Console.WriteLine(i);
+             }
+
+             numList.RemoveAt(0);
+             Console.WriteLine("Удаление элемента");
+             foreach (int i in numList)
+             {
+                 Console.WriteLine(i);
+             }*/
+
+            List<Point> pList = new List<Point>{ new Point(1, 3, '*'),  new Point(4, 5, '-'),
+            new Point(7, 6, '&'), new Point(4, 3, '%') };
+
+            pList.Remove(pList[0]);
+
+            pList.Insert(2, new Point(5, 9, '$'));
+
+            for (int i = 0; i < pList.Count; i++)
+            {
+                int num = i + 1;
+                Console.WriteLine("Точка " + num + " имеет координаты: (x" + pList[i].x + "), y(" + pList[i].y +
+                    ") и символ (" + pList[i].sym +")");
+            }
+
+
 
             Console.ReadLine();
         }
